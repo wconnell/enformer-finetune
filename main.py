5150bc3c6ff747508486805715cbff8eb7f9e0ae
Aaron <aaronwenteler@gmail.com>
2023-12-08 15:29:52 +0000

    add todos

--- main.py
+++ main.py
@@ -3,19 +3,18 @@ from lightning.pytorch.cli import LightningCLI
 from eft.models import EnformerTX
 from eft.data import EnformerTXDataModule
 
+
 torch.set_float32_matmul_precision('medium')
+
 
 def cli_main():
     cli = LightningCLI(EnformerTX, EnformerTXDataModule)
-    # note: don't qcall fit!!
+    # note: don't call fit!!
 
 
 if __name__ == "__main__":
     cli_main()
 
     # TODO: We only have 40 GB VRAM. Figure out how many VRAM we need, and try to fix appropiately.
-    #  - We can try to use a smaller batch size.
+    #  - Use DNAse seq (K562) instead of the RNA seq (GM12878).
     #  - Parallelise: https://pytorch-lightning.readthedocs.io/en/1.1.8/multi_gpu.html#model-parallelism
-
-
-
